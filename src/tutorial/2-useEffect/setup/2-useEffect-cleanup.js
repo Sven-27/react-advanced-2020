8666609234d26c3c2e5e588fbc8bb8277422909d
Sven Notermans <sven.notermans@live.nl>
2021-03-26 11:57:57 +0100

    completed useeffects

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,25 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth)
+
+const checkSize = () => {
+  setSize(window.innerWidth)
+}
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    return  ()=> {
+      //clean up eventlistener is also good for toggling between pages
+      console.log('cleanup')
+      window.removeEventListener('resize', checkSize)
+    }
+  },[]) //this works when you stay on page.
+  console.log(size)
+  return <>
+  <h1>window</h1>
+  <h2>{size} PX</h2>
+  </>;
 };
 
 export default UseEffectCleanup;
